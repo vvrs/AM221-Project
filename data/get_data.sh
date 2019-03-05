e93a1f986f06cd771ee9454d2a0414d4a151894e
vishnu <vishnu.rudrasamudram@gmail.com>
2019-03-04 20:06:33 -0500

    Added mnist source to get_data

--- data/get_data.sh
+++ data/get_data.sh
@@ -4,3 +4,8 @@ tar -xzvf cifar-10-python.tar.gz
 rm cifar-10-python.tar.gz 
 mkdir cifar-10-python
 mv cifar-10-batches-py cifar-10-python
+
+# Get MNIST
+
+wget yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
